5d89ea979db10b0219d9a2af9ff5fd78cf74f876
JIshil0070-glitch <prototype0070@gmail.com>
2025-12-09 13:21:14 +0530

    problem report partial

---
+++ PrototypeModel/Presentation/views/ProblemReportSendView.swift
@@ -0,0 +1,18 @@
+//
+//  ProblemReportSendView.swift
+//  PrototypeModel
+//
+//  Created by Jishil Krishna  on 09/12/25.
+//
+
+import SwiftUI
+
+struct ProblemReportSendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProblemReportSendView()
+}
